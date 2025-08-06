48790c2fc31270bd9e37255e702011fe975667f7
godwins-code <godwins.techoffice@gmail.com>
2025-08-06 12:59:16 +0300

    Deleted Files

--- user input.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-name = input("NAME: ")
-year_of_birth = int(input("YEAR OF BIRTH: "))
-current_year = datetime.datetime.now().year
-age = (current_year) - (year_of_birth)
-
-print(f'{name}', age, 'years old')
--- variables.py
+++
@@ -1,5 +0,0 @@
-computers = "MacBook"
-unit_price = 150000.00
-quantity = 20
-
-print(computers, unit_price, f"Quantity: {quantity}", sep="\n")
